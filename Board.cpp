66ff99b98c4bf11c14679fd8e6d04be1d329df49
soeviturek <wuyc.james@gmail.com>
2022-05-04 01:55:28 +1000

    fixed a little bit of board

--- Board.cpp
+++ Board.cpp
@@ -2,10 +2,24 @@
 
 Board::Board(){
     for(int i = 0; i < DEFAULT_DIMENSION; ++i){
-        std::vector<char> rowVector;
+        std::vector<Tile*> rowVector;
         for(int j = 0; j < DEFAULT_DIMENSION; ++j){
-            rowVector.push_back(0);
+            rowVector.push_back(nullptr);
         }
         board.push_back(rowVector);
     }
+}
+std::string Board::printBoard(){
+    std::string boardStatus = "";
+    for(int i = 0; i < DEFAULT_DIMENSION; ++i){
+        boardStatus += ""; // convert number to letter
+        //string.append(char)
+        for(int j = 0; j < DEFAULT_DIMENSION; ++j){
+            if(board[i][j] == nullptr){
+                boardStatus += "   ";
+            }
+        }
+        boardStatus += "|\n";
+    }
+    return boardStatus;
 }
