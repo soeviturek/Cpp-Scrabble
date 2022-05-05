895907b6ff987d6a0747fdcd67e3ff8e38bae2aa
soeviturek <wuyc.james@gmail.com>
2022-05-05 23:09:45 +1000

    finish board

--- Board.cpp
+++ Board.cpp
@@ -9,17 +9,40 @@ Board::Board(){
         board.push_back(rowVector);
     }
 }
-std::string Board::printBoard(){
-    std::string boardStatus = "";
+Board::~Board(){
     for(int i = 0; i < DEFAULT_DIMENSION; ++i){
-        boardStatus += ""; // convert number to letter
+        for(int j = 0; j < DEFAULT_DIMENSION; ++j){
+            if(board[i][j] != nullptr){
+                delete board[i][j];
+            }
+        }
+    }
+}
+std::string Board::printBoard(){
+    std::string boardStatus = "     0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  \n   -------------------------------------------------------------\n";
+    for(int i = 0; i < DEFAULT_DIMENSION; ++i){
+        char add = 'A'+i;
+        boardStatus += add; // convert number to letter
+        boardStatus += "  |";
         //string.append(char)
         for(int j = 0; j < DEFAULT_DIMENSION; ++j){
             if(board[i][j] == nullptr){
-                boardStatus += "   ";
+                boardStatus += "   |";
+            }else{
+                Tile* tile = board[i][j];
+                boardStatus += ' ';
+                boardStatus +=tile->letter;
+                boardStatus += " |";
             }
         }
-        boardStatus += "|\n";
+        boardStatus += "\n";
     }
     return boardStatus;
+}
+
+char Board::getSquare(int row, int col){
+    return board[row][col]->letter;
+}
+void Board::placeTile(Tile* tile, int row, int col){
+    board[row][col] = tile;
 }
--- scrabble.cpp
+++ scrabble.cpp
@@ -17,28 +17,39 @@ int main(void) {
    // delete testMenu;
    
    //test tile bag
-   TileBag* tileBag = new TileBag();
+   // TileBag* tileBag = new TileBag();
 
-   //print out tiles in the bag
-   LinkedList* test = tileBag->getTileBag();
-   std::cout<<"Size of Bag: "<<test->getSize() << "\n";
-   std::cout << "print tiles in the list\n";
-   for(int i = 0; i < test->getSize(); i++){
-      Tile* tile = test->get(i);
-      std::cout << tile->letter <<"-"<<tile->value<<"\n";
-   }
+   // //print out tiles in the bag
+   // LinkedList* test = tileBag->getTileBag();
+   // std::cout<<"Size of Bag: "<<test->getSize() << "\n";
+   // std::cout << "print tiles in the list\n";
+   // for(int i = 0; i < test->getSize(); i++){
+   //    Tile* tile = test->get(i);
+   //    std::cout << tile->letter <<"-"<<tile->value<<"\n";
+   // }
    
-   //generate hand 
-   Hand* hand = tileBag->initiateHand();
-   std::cout << hand->printHand();
-   Tile* tempTile = new Tile();
-   tempTile->letter = 'A';
-   tempTile->value = 1;
-   hand->delTile(tempTile);
-   std::cout << hand->printHand();
-   delete tileBag;
-   delete hand;
-   delete tempTile;
+   // //generate hand 
+   // Hand* hand = tileBag->initiateHand();
+   // std::cout << hand->printHand();
+   // Tile* tempTile = new Tile();
+   // tempTile->letter = 'A';
+   // tempTile->value = 1;
+   // hand->delTile(tempTile);
+   // std::cout << hand->printHand();
+   // delete tileBag;
+   // delete hand;
+   // delete tempTile;
+
+   Board* board = new Board();
+   Tile* tile = new Tile();
+   tile->letter = 'Q';
+   tile->value = 99;
+   std::cout<< "Board:\n" << board->printBoard();
+   board->placeTile(tile,5,5);
+   std::cout<< "Place: " << board->getSquare(5,5);
+   std::cout<< "Board:\n" << board->printBoard();
+   delete board;
+   delete tile;
 
    
    return EXIT_SUCCESS;
